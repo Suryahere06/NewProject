4ce457511811f7f5fbe0abb5c8514c19e5c97fc5
Suryahere06 <58037576+Suryahere06@users.noreply.github.com>
2020-03-02 20:49:29 -0500

    Initial commit

---
+++ src/NewProject1/stringEx.java
@@ -0,0 +1,44 @@
+package NewProject1;
+
+import java.util.*;
+import java.lang.String;
+
+public class stringEx {
+	
+	//public stringEx(String s)
+	{
+		
+	}
+	Public stringEx (Char []c)
+	{
+		
+	}
+	Public stringEx ()
+	{
+		
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            char []c=new char[5];
+                    c[0]='d';
+            		c[1]='a';
+            		c[2]='t';
+            		c[3]='a';
+			String s= new String(c);  //s=a1 , c=b1
+			System.out.println(s);   
+			//s=a1
+			s="data"              //data = x1  , s=x1
+			System.out.println(s);
+
+			s= new String("data");//s=y1
+			System.out.println(s);
+
+			s=s.replace("d",""); //s=z1
+			s=s+"1";
+			System.out.println(s);
+
+	}
+
+}
